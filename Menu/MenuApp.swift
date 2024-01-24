cc728299350bff086b893fbf271d632c987f18ca
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-24 11:15:52 -0700

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Matthew Dembinski on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
