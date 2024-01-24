486dccac682a605bf1d6c0ea2befc1f7ecab4bae
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-24 16:48:09 -0700

    ADD: Finished Menu App Tutorial

---
+++ Menu/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  Menu
+//
+//  Created by Matthew Dembinski on 1/24/24.
+//
+
+import Foundation
---
+++ Menu/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Matthew Dembinski on 1/24/24.
+//
+
+import Foundation
